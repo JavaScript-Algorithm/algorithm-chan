cdcf50458bf598d7079171541b2d487420fa1ae9
ChanLee <dlcks113@gmail.com>
2022-08-03 11:30:16 +0900

    행렬 테두리 회전하기 약간 수정

--- programmers/Lv2/행렬 테두리 회전하기.js
+++ programmers/Lv2/행렬 테두리 회전하기.js
@@ -1,9 +1,9 @@
 function rotate(map, query) {
     // 순회 및 큐에 대입
-    // 1. y1 ~ y2 (x1)
-    // 2. x1 ~ x2 (y2)
-    // 3. y2 ~ y1 (x2)
-    // 4. x2 ~ x1 (y1)
+    // 1. y1 ~ y2 (x1 고정)
+    // 2. x1 ~ x2 (y2 고정)
+    // 3. y2 ~ y1 (x2 고정)
+    // 4. x2 ~ x1 (y1 고정)
     const queue = [];
     const [x1, y1, x2, y2] = query.map(e => e - 1);
     
@@ -13,8 +13,7 @@ function rotate(map, query) {
     for (let r = x2 - 1; r >= x1 + 1; r--) queue.push(map[r][y1]);
     
     const min_val = Math.min(...queue);
-    const end = queue.pop();
-    queue.unshift(end);
+    queue.unshift(queue.pop());
     
     // 회전 
     for (let c = y1; c <= y2; c++) map[x1][c] = queue.shift();
@@ -27,15 +26,9 @@ function rotate(map, query) {
 
 function solution(rows, columns, queries) {
     // 배열 생성
-    const map = Array.from( {length: rows}, () => Array(columns).fill(0))
-    
-    let val = 1;
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
-            map[i][j] = val;
-            val++;
-        }
-    }
+    const map = [...Array(rows)]
+                    .map((_, r) => [...Array(columns)]
+                    .map((_, c) => r * columns + c + 1));
     
     const ans = [];
     queries.forEach(query => {
